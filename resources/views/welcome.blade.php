d032075ea5035a87e039fa65975844bf02f405fc
UmerYou <umeryou@outlook.com>
2024-10-14 14:10:49 +0500

    home page button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,8 +68,9 @@
     <div class="background"></div>
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <h1 class="gold">Spin To Win Nims</h1>
         <a href="{{ route('spinner-game') }}">
-        <img style="text-align: center; position: relative; left: 37%;" src="{{ asset('account/landing_page_images/assets/spinner_button.png') }}" alt="">
+        <img style="text-align: center; position: relative; left: 37%; width: 27%;" src="{{ asset('account/landing_page_images/assets/spinner_button.png') }}" alt="">
         </a>
     </div>
 </body>
