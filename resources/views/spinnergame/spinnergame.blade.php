2210f732c65b07ddc03d04a923a6d675f24c6183
UmerYou <umeryou@outlook.com>
2024-10-18 13:04:26 +0500

    celebration

--- resources/views/spinnergame/spinnergame.blade.php
+++ resources/views/spinnergame/spinnergame.blade.php
@@ -89,6 +89,7 @@
                     <input type="hidden" value="" id="reward_tokens">
                     {{-- <button class="pop-up-btn" id="closePopup">Activate Bonus</button> --}}
                 </div>
+                <audio src="https://assets.codepen.io/1256430/whistle.mp3" id="sound_" preload="auto" hidden></audio>
 
                 <!-- START RANKING -->
 
@@ -1341,4 +1342,68 @@ $(document).ready(function() {
         });
     });
 });
+</script>
+<script>
+    const sound = document.getElementById("sound_");
+const triggers = document.querySelectorAll(".pop-up-content");
+const defaults = {
+  disableForReducedMotion: true
+};
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(200 * particleRatio)
+    })
+  );
+}
+
+function confettiExplosion(origin) {
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+    origin
+  });
+  fire(0.2, {
+    spread: 60,
+    origin
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    origin
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    origin
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+    origin
+  });
+}
+
+Array.from(triggers).forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    const rect = trigger.getBoundingClientRect();
+    const center = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    };
+    const origin = {
+      x: center.x / window.innerWidth,
+      y: center.y / window.innerHeight
+    };
+
+    if (sound) {
+      sound.currentTime = 0;
+      sound.play();
+    }
+    confettiExplosion(origin);
+  });
+});
+
 </script>
