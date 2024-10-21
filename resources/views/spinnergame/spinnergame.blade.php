7fb6185668a6645656aa2af15b82a1b532ec064b
UmerYou <umeryou@outlook.com>
2024-10-21 16:51:36 +0500

    spell of leaderboard

--- resources/views/spinnergame/spinnergame.blade.php
+++ resources/views/spinnergame/spinnergame.blade.php
@@ -249,7 +249,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content modal_body_invite">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="walletModalLabel">NIMS Leader Board</h5>
+                    <h5 class="modal-title" id="walletModalLabel">NIMS Leaderboard</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
