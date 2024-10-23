5fbcfa828a22c5aa2e481c164ee8c2b87609aba6
UmerYou <umeryou@outlook.com>
2024-10-23 17:15:30 +0500

    change the onload modal

--- resources/views/partials/OnLoadReferalModal.blade.php
+++ resources/views/partials/OnLoadReferalModal.blade.php
@@ -6,7 +6,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body referal_modal_image">
-        <img src="{{ asset('account/landing_page_images/assets/referal_spin.jpg') }}" alt="">
+        <img src="{{ asset('account/landing_page_images/assets/referal_get3_spin.gif') }}" alt="">
       </div>
       <!-- <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
